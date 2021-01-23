077cc3a29a4cc0d1c55914607a85a9c6d371bda1
AnnaDan1347 <52747391+AnnaDan1347@users.noreply.github.com>
2021-01-23 23:42:17 +0300

    add cout << ;  if there is no powers of two in this array

--- degree2.cpp
+++ degree2.cpp
@@ -6,7 +6,7 @@
 
 
 #define NUMBER_OF_ELEMENTS 100
-#define MAX_ELEMENT 1000
+#define MAX_ELEMENT 100
 
 using namespace std;
 
@@ -25,6 +25,8 @@ int main(){
 	
 	cout << endl;	
 	
+	int bingo = 0;
+	
 	for (int i = 0; i < NUMBER_OF_ELEMENTS; i++){
 		int degree = 0;
 		double temp = 0.0;
@@ -32,13 +34,16 @@ int main(){
 			temp = pow(2,degree);
 			if (temp == array[i]) {
 				cout << "a[" << i << "] = 2^" << degree << " = " << array[i] << endl;
+				bingo++;
 				break;
 			}
 			degree++;
 		}
 	}
 	
+	if (bingo == 0){
+		cout << "There is no powers of two in this array ";
+	}
+	
 	return 0;
 }
-
-
